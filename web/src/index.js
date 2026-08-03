da4dd481b7471f340f4754d323bbf40ec926b895
Marvello Oni <marvello.oni@gmail.com>
2026-08-03 16:35:17 +0700

    Change logo to use png instead of SVG

--- web/src/index.js
+++ web/src/index.js
@@ -12,6 +12,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "../views"));
+app.use(express.static(path.join(__dirname, "../public")));
 app.use(express.json({ limit: "25mb" }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
